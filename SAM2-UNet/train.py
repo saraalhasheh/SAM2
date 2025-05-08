76255a3908aa7e65cf611daf564b97a216dbb544
yazan-alrousan <yazan278243@hotmail.com>
2025-05-08 17:47:50 +0300

    Update code with modern PyTorch loss functions and push all files

--- SAM2-UNet/train.py
+++ SAM2-UNet/train.py
@@ -30,7 +30,7 @@ args = parser.parse_args()
 
 def structure_loss(pred, mask):
     weit = 1 + 5*torch.abs(F.avg_pool2d(mask, kernel_size=31, stride=1, padding=15) - mask)
-    wbce = F.binary_cross_entropy_with_logits(pred, mask, reduce='none')
+    wbce = F.binary_cross_entropy_with_logits(pred, mask, reduction='none')
     wbce = (weit*wbce).sum(dim=(2, 3)) / weit.sum(dim=(2, 3))
     pred = torch.sigmoid(pred)
     inter = ((pred * mask)*weit).sum(dim=(2, 3))
