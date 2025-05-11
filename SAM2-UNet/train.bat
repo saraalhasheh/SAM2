40afb2229aa95f533ac50c25da7ed601598a1530
Hassan Aqrabawi <hasan.aqrabawi@bahcesehir.edu.tr>
2025-05-12 00:09:34 +0300

    Initial commit

--- SAM2-UNet/train.bat
+++
@@ -1,8 +0,0 @@
-python train.py ^
---hiera_path pretrained/sam2_hiera_large.pt ^
---train_image_path data/images/train ^
---train_mask_path data/masks/train ^
---save_path checkpoints ^
---epoch 20 ^
---lr 0.001 ^
---batch_size 12
